4daa3990c52e5f7c14f6c06ed0d9c4dca86c1f90
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-06-17 12:13:15 +0330

    edit task(need modal)

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,14 @@
+import * as React from "react";
+
 const Task = ({ setTasks, tasks, index }) => {
   const task = tasks[index];
   const initialDone = Boolean(tasks[index].done);
   const newTasks = [...tasks];
 
-  const changeHandle = () => {
+  const [taskName, setTaskName] = React.useState(task.name);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const checkHandle = () => {
     newTasks[index].done = !tasks[index].done;
     setTasks(newTasks);
   };
@@ -16,10 +21,28 @@ const Task = ({ setTasks, tasks, index }) => {
     setTasks(newTasks);
   };
 
+  const editTask = () => {
+    setIsOpen((prevS) => !prevS);
+  };
+
+  const editSubmit = (ev) => {
+    ev.preventDefault();
+    newTasks[index].name = taskName;
+    setTasks(newTasks);
+    setIsOpen(false)
+  };
+
   return (
     <div>
-      <input type="checkbox" checked={initialDone} onChange={changeHandle} /> {task.name}
+      <input type="checkbox" checked={initialDone} onChange={checkHandle} /> {task.name}
       <button onClick={(ev) => deleteTask(ev)}>X</button>
+      <button onClick={editTask}>{isOpen ? "X" : "edit"}</button>
+      {isOpen && (
+        <form onSubmit={(ev) => editSubmit(ev)}>
+          <input value={taskName} onChange={({ target }) => setTaskName(target.value)} />
+          <button type="submit">submit</button>
+        </form>
+      )}
     </div>
   );
 };
