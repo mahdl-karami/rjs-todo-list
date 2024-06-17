6b52ca0eb33a158395a9bdf26e18084b48b995fc
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-06-17 11:54:59 +0330

    delete tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,15 +2,24 @@ const Task = ({ setTasks, tasks, index }) => {
   const task = tasks[index];
   const initialDone = Boolean(tasks[index].done);
   const newTasks = [...tasks];
-  
+
   const changeHandle = () => {
     newTasks[index].done = !tasks[index].done;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (ev) => {
+    if (index > -1) {
+      // only splice array when item is found
+      newTasks.splice(index, 1); // 2nd parameter means remove one item only
+    }
     setTasks(newTasks);
   };
 
   return (
     <div>
       <input type="checkbox" checked={initialDone} onChange={(ev) => changeHandle(ev)} /> {task.name}
+      <button onClick={(ev) => deleteTask(ev)}>X</button>
     </div>
   );
 };
