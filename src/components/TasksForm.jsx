7538b16695819b5d2d8d5d81bf437225f7d13775
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-06-17 08:32:20 +0330

    clear task form onSubmit

--- src/components/TasksForm.jsx
+++ src/components/TasksForm.jsx
@@ -1,13 +1,22 @@
+import * as React from "react";
+
 const TasksForm = ({ setTasks }) => {
+  const [value, setValue] = React.useState("");
+
+  const changeHandle = (ev) => {
+    setValue(ev.target.value);
+  };
+
   const formHandle = (ev) => {
     ev.preventDefault();
     const input = ev.target[0];
     setTasks((prevS) => [...prevS, { name: input.value, done: false }]);
+    setValue("");
   };
 
   return (
     <form onSubmit={(ev) => formHandle(ev)}>
-      <input type="text" />
+      <input type="text" value={value} onChange={(ev) => changeHandle(ev)} />
       <button type="submit">Add</button>
     </form>
   );
