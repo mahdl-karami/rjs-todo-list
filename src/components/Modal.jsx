c09529c0b477c91eeed6642bf838a2dfedc76398
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-06-18 18:42:38 +0330

    clean code (modal.jsx)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,28 @@
-const Modal = ({ edit, taskName, setTaskName, editSubmit, formHandle, value, changeHandle }) => {
-  // if edit == false (not defined), this modal is for create new task
+//! this modal is for create or edit a task
+//! if edit == false (not defined) this section is used to create
+const Modal = (props) => {
+  //* edit destructure
+  const { edit, taskName, setTaskName, editSubmit } = props;
+  //* create destructure
+  const { value, formHandle, changeHandle } = props;
+
+  //* modal functions
+  const submitHandle = (ev) => {
+    edit && editSubmit(ev);
+    !edit && formHandle(ev);
+  };
+  const changeHandle2 = (ev) => {
+    edit && setTaskName(ev.target.value);
+    !edit && changeHandle(ev);
+  };
+
   return (
-    <div style={{ background: "#ccc" }}>
-      <h2>{edit ? "edit task" : "create new task"}</h2>
-      {edit && (
-        <form onSubmit={(ev) => editSubmit(ev)}>
-          <input value={taskName} onChange={({ target }) => setTaskName(target.value)} />
-          <button type="submit">submit</button>
-        </form>
-      )}
-      {!edit && (
-        <form onSubmit={(ev) => formHandle(ev)}>
-          <input type="text" value={value} onChange={(ev) => changeHandle(ev)} />
-          <button type="submit">Add</button>
-        </form>
-      )}
+    <div className="modal">
+      <h3 className="modal-title">{edit ? "edit task" : "create new task"}</h3>
+      <form onSubmit={(ev) => submitHandle(ev)}>
+        <input type="text" value={edit ? taskName : value} onChange={(ev) => changeHandle2(ev)} placeholder="enter task name" />
+        <button type="submit">{edit ? "Edit" : "Add"}</button>
+      </form>
     </div>
   );
 };
